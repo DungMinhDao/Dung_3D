085850740915b85a8522e0f1cc054b27198f835b
bladeemon35 <52767683+bladeemon35@users.noreply.github.com>
2019-07-11 09:30:32 +0700

    Add files via upload

--- Dung_home/header-navigation-area.php
+++ Dung_home/header-navigation-area.php
@@ -8,81 +8,34 @@
                                 <div class='header-navigation__nav position-static'>
                                     <nav>
                                         <ul>
-                                            <li class='has-children has-children--multilevel-submenu'>
+                                            <li class='has-children--multilevel-submenu'>
                                                 <a href='index.php'>TRANG CHỦ</a>
-                                                <ul class='submenu'>
-                                                    <li><a href='index.php'>Homepage One</a></li>
-                                                    <li><a href='index-2.php'>Homepage Two</a></li>
-                                                </ul>
                                             </li>
-                                            <li><a href='about.html'>GIỚI THIỆU</a></li>
+                                            <li><a href='about.php'>GIỚI THIỆU</a></li>
 
                                             <li class='has-children has-children--multilevel-submenu'>
-                                                <a href='service.html'>DỊCH VỤ</a>
+                                                <a href='service.php'>DỊCH VỤ</a>
                                                 <ul class='submenu'>
-                                                    <li><a href='service.html'>Service Page</a></li>
-                                                    <li><a href='service-details.html'>Service Details Left Sidebar</a></li>
-                                                    <li><a href='service-details-right-sidebar.html'>Service Details Right Sidebar</a></li>
-                                                    <li><a href=''>HỘP QUÀ TẶNG</a></li>
-                                                    <li><a href=''>HỘP QUÀ TẾT</a></li>
-                                                    <li><a href=''>HỘP RƯỢU CAO CẤP</a></li>
-                                                    <li><a href=''>HỘP SÓNG</a></li>
-                                                    <li><a href=''>IN CATALOGUE</a></li>
-                                                    <li><a href=''>IN HỘP CỨNG CAO CẤP</a></li>
-                                                    <li><a href=''>IN HỘP GIẤY TÚI GIẤY</a></li>
+                                                    <li><a href=''>Hộp quà tặng</a></li>
+                                                    <li><a href=''>Hộp quà Tết</a></li>
+                                                    <li><a href=''>Hộp rượu cao cấp</a></li>
+                                                    <li><a href=''>Hộp sóng</a></li>
+                                                    <li><a href=''>In catalogue</a></li>
+                                                    <li><a href=''>In hộp cứng cao cấp</a></li>
+                                                    <li><a href=''>In hộp giấy túi giấy</a></li>
                                                 </ul>
                                             </li>
 
                                             <li class='has-children has-children--multilevel-submenu'>
-                                                <a href='project.html'>PROJECT</a>
+                                                <a href='project.php'>SẢN PHẨM</a>
                                                 <ul class='submenu'>
-                                                    <li><a href='project.html'>Project Page</a></li>
-                                                    <li><a href='project-details.html'>Project Details</a></li>
-                                                    <li class='has-children'>
-                                                        <a href='#'>Submenu Level One</a>
-                                                        <ul class='submenu'>
-                                                            <li class='has-children'>
-                                                                <a href='#'>Submenu Level Two</a>
-                                                                <ul class='submenu'>
-                                                                    <li><a href='#'>Submenu Level Three</a></li>
-                                                                    <li><a href='#'>Submenu Level Three</a></li>
-                                                                    <li><a href='#'>Submenu Level Three</a></li>
-                                                                    <li><a href='#'>Submenu Level Three</a></li>
-                                                                </ul>
-                                                            </li>
-                                                            <li><a href='#'>Submenu Level Two</a></li>
-                                                            <li><a href='#'>Submenu Level Two</a></li>
-                                                            <li><a href='#'>Submenu Level Two</a></li>
-                                                        </ul>
-                                                    </li>
-                                                    <li class='has-children'>
-                                                        <a href='#'>Submenu Level One</a>
-                                                        <ul class='submenu'>
-                                                            <li class='has-children'>
-                                                                <a href='#'>Submenu Level Two</a>
-                                                                <ul class='submenu'>
-                                                                    <li><a href='#'>Submenu Level Three</a></li>
-                                                                    <li><a href='#'>Submenu Level Three</a></li>
-                                                                    <li><a href='#'>Submenu Level Three</a></li>
-                                                                    <li><a href='#'>Submenu Level Three</a></li>
-                                                                </ul>
-                                                            </li>
-                                                            <li><a href='#'>Submenu Level Two</a></li>
-                                                            <li><a href='#'>Submenu Level Two</a></li>
-                                                            <li><a href='#'>Submenu Level Two</a></li>
-                                                        </ul>
-                                                    </li>
+                                                    <li><a href='project.php'>Tổng quan</a></li>
+                                                    <li><a href='project-details.php'>Chi tiết</a></li>
                                                 </ul>
                                             </li>
 
-                                            <li class='has-children has-children--multilevel-submenu'>
-                                                <a href='blog-left-sidebar.html'>TIN TỨC</a>
-                                                <ul class='submenu'>
-                                                    <li><a href='blog-left-sidebar.html'>Blog Left Sidebar</a></li>
-                                                    <li><a href='blog-right-sidebar.html'>Blog Right Sidebar</a></li>
-                                                    <li><a href='blog-details-left-sidebar.html'>Blog Details Left Sidebar</a></li>
-                                                    <li><a href='blog-details-right-sidebar.html'>Blog Details Right Sidebar</a></li>
-                                                </ul>
+                                            <li class='has-children--multilevel-submenu'>
+                                                <a href='blog-left-sidebar.php'>TIN TỨC</a>
                                             </li>
                                             <li><a href='contact.php'>LIÊN HỆ</a></li>
                                         </ul>
